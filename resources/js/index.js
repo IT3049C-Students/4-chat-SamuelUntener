ec059068356e4da3155e8c689828fbbae6c7ebce
SamuelUntener <75641765+SamuelUntener@users.noreply.github.com>
2022-02-20 13:55:48 -0500

    storage lab done

--- resources/js/index.js
+++ resources/js/index.js
@@ -78,6 +78,43 @@ sendButton.addEventListener("click", function(sendButtonClickEvent) {
   const sender = nameInput.value;
   const message = myMessage.value;
 
+  validate();
+
   sendMessages(sender,message);
   myMessage.value = "";
+
 });
+
+function validate() {
+    if(validateFields){
+        localStorage.setItem('sender', userInput[0].value);
+      }
+   }
+  
+  function validateFields(){
+   var retVal = false;
+   var userInput = document.querySelectorAll("nameInput");
+   for(var i = 0; i < userInput.length; i++) {
+      if(userInput[i].value === "") {
+        alert("Please fill all required fields!");
+        break;
+      }else{
+        retval = true;
+      }
+    }
+   return retval;
+  }
+
+  function SaveUsername(){
+      localStorage.setItem('nameInput', JSON.stringify(nameInput))
+  }
+
+  function ChangeUsername(){
+      localStorage.removeItem(nameInput)
+      localStorage.setItem('nameInput', JSON.stringify(nameInput))
+  }
+
+  function darkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
